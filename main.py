a74703246ce0c89200c33a74ceefdeb2080bf3e6
Bakharev Egor <eg.baharev@gmail.com>
2026-02-16 22:01:32 +0300

    Создал файл product.py: -class MyError (для ошибок) -class ProductApp (модуль заполнения) -class ProductApp (модуль для частичного обновления)
    class Product (методы для товара):
    -get_product_by_id
    -product_add (добавить)
    -product_remove (
    -product_receive (получить)
    -product_change (частичное обновление)
    -product_list (список товаров)
    
    файл main.py
    Доделал эндпоинты для товара

--- main.py
+++ main.py
@@ -1,50 +1,9 @@
-from typing import List
-
 from fastapi import FastAPI, HTTPException, status
-from datetime import datetime, timedelta
-from pydantic import BaseModel, Field
-
-
-# Подправить класс чуть позже
-class Product(BaseModel):
-    id: int  # авто
-    sku: str  # нужна уникальная строка
-    name: str
-    escription: str = "Описание товара"  # может быть пустой
-    price: float = 0.0  # не меньше 0.0
-    created_at: datetime  # дата/время авто
-
-    def product_list(self):
-
-
-
-# переименовать переменные
-def random_products():
-    # Создаём список из 10 объектов Product
-    products: List[Product] = []
-    base_time = datetime.now()
-
-    for i in range(1, 21):
-        product = Product(
-            id=i,  # задаём уникальный id
-            name=f"Товар {i}",
-            sku=f"SKU-{i:03d}",  # например, SKU-001
-            description=f"Подробное описание товара номер {i}",
-            price=round(100.0 + i * 15.5, 2),  # разные цены
-            created_at=base_time - timedelta(days=i)  # разные даты
-        )
-        products.append(product)
-    return products
-
-
-products = random_products()
+from product import Product, ProductUpdate, MyError, ProductApp
 
 app = FastAPI()
 """
 Правила которые нужно сделать
-
-1. Нельзя создать товар с таким же sku, если он уже есть.
-
 2. Нельзя сделать OUT, если на складе не хватает товара.
 
 3. Остаток товара не может стать отрицательным.
@@ -53,54 +12,43 @@ app = FastAPI()
 """
 
 
-# исправить чтобы в запросе не было id и created_at
-"""
-доделать 
-
-Ответ: созданный товар (с id и created_at).
-
-Ошибки:
-sku уже существует → 400 или 409
-"""
 @app.post("/products")
-def product_create(product: Product):
+def product_create(product: ProductApp):
 
-    return products.append(product)
+    try:
+        new_product = Product.product_add(product)
+        return new_product
+    except MyError as my_error:
+        raise HTTPException(status_code=my_error.code, detail=my_error.message)
 
 
 @app.get("/products")
-def products_list(limit: int = 20, offset: int = 0, search: str = None): # limit and search сделать
-    if search:
-        search_lower = search.lower()
-        result = []
-
-        for prod in products:
-            if search_lower in prod.sku.lower() or search_lower in prod.name.lower():
-                result.append(prod)
-        return result[offset:limit]
-
-    return products[offset:limit]
+def products_list(limit: int = 20, offset: int = 0, search: str = None):
+    return Product.product_list(limit=limit, offset=offset, search=search)
 
 
 @app.get("/products/{id}")
-def product_view(id: int):
-    for product in products:
-        if product.id == id:
-            return product
+def product_view(pid: int):
 
-    raise HTTPException(status_code=404, detail=f"Товара нет {id}")
+    try:
+        return Product.product_receive(pid)
+    except MyError as my_error:
+        raise HTTPException(status_code=my_error.code, detail=my_error.message)
 
 
 @app.patch("/products/{id}")
-def product_patch(id: int):
-    ...
+def product_patch(pid: int, product_update: ProductUpdate):
+
+    try:
+        return Product.product_change(pid, product_update=product_update)
+    except MyError as my_error:
+        raise HTTPException(status_code=my_error.code, detail=my_error.message)
 
 
 @app.delete('/products/{id}', status_code=status.HTTP_204_NO_CONTENT)
-def product_delete(id: int):
-    for product in products:
-        if product.id == id:
-            products.remove(product)
-            return
+def product_delete(pid: int):
 
-    raise HTTPException(status_code=404, detail="Товара нет")
+    try:
+        Product.product_remove(pid)
+    except MyError as my_error:
+        raise HTTPException(status_code=my_error.code, detail=my_error.message)
---
+++ product.py
@@ -0,0 +1,118 @@
+from datetime import datetime, timedelta
+from typing import List, Optional
+from pydantic import BaseModel, Field
+
+
+class MyError(Exception):
+    def __init__(self, code, message="Возникла ошибка"):
+        self.code = code
+        self.message = message
+        super().__init__(self.message)
+
+
+# Модель которую заполняют
+class ProductApp(BaseModel):
+    sku: str
+    name: str
+    description: Optional[str] = None
+    price: float = Field(0.0, ge=0.0)
+
+
+# Модель для частичного обновления (все поля необязательные)
+class ProductUpdate(BaseModel):
+    name: Optional[str] = None
+    description: Optional[str] = None
+    price: Optional[float] = None
+
+
+class Product(BaseModel):
+    id: int
+    sku: str
+    name: str
+    description: str = "Описание товара"
+    price: float = Field(0.0, ge=0.0)
+    created_at: datetime = Field(default_factory=datetime.now)
+
+    @staticmethod
+    def get_product_by_id(pid: int):
+        for product in products_list:
+            if product.id == pid:
+                return product
+        raise MyError(code=404, message='Товара нет')
+
+    @classmethod
+    def product_add(cls, app: ProductApp):
+
+        for prod in products_list:
+            if app.sku == prod.sku:
+                raise MyError(code=400, message=f"{app.sku} уже существует")
+
+        product = cls(
+            id=len(products_list) + 1,
+            name=app.name,
+            sku=app.sku,
+            description=app.description,
+            price=app.price
+        )
+
+        products_list.append(product)
+        return product
+
+    @classmethod
+    def product_remove(cls, pid: int):
+        product = cls.get_product_by_id(pid=pid)
+        products_list.remove(product)
+
+    @classmethod
+    def product_receive(cls, pid: int):
+        return cls.get_product_by_id(pid=pid)
+
+    @classmethod
+    def product_change(cls, pid: int, product_update: ProductUpdate):
+
+        if isinstance(product_update.price,  (int, float)) and product_update.price < 0:
+            raise MyError(code=400, message='Цена меньше 0.0')
+
+        existing_product = cls.get_product_by_id(pid=pid)
+        update_data = product_update.model_dump(exclude_unset=True)
+
+        for field, value in update_data.items():
+            setattr(existing_product, field, value)
+
+        return existing_product
+
+    @classmethod
+    def product_list(cls, limit: int = 20, offset: int = 0, search: str = None):
+
+        if search:
+            search_lower = search.lower()
+            result = []
+
+            for prod in products_list:
+                if search_lower in prod.sku.lower() or search_lower in prod.name.lower():
+                    result.append(prod)
+            return result[offset: offset + limit]
+
+        return products_list[offset: offset + limit]
+
+
+def random_products():
+    products: List[Product] = []
+    base_time = datetime.now()
+
+    for i in range(1, 3):
+        product = Product(
+            id=i,  # задаём уникальный id
+            name=f"Товар {i}",
+            sku=f"SKU-{i:03d}",  # например, SKU-001
+            description=f"Подробное описание товара номер {i}",
+            price=round(100.0 + i * 15.5, 2),  # разные цены
+            created_at=base_time - timedelta(days=i)  # разные даты
+        )
+        products.append(product)
+    return products
+
+
+products_list = random_products()
+print(products_list)
+
